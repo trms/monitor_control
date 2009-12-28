5f9d264a542a5823c659355055bce6ea79bf4196
JJ Parker <jj@trms.com>
2009-12-28 13:34:17 -0600

    bugzid: 4774 - Added mDNS auto discovery

--- MonitorControl/Program.cs
+++ MonitorControl/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Windows.Forms;
+using ZeroconfService;
+using System.Net;
 
 namespace TRMS.CarouselMonitorControl
 {
@@ -12,9 +15,15 @@ namespace TRMS.CarouselMonitorControl
         [STAThread]
         static void Main()
         {
+            //Register with bonjour
+            NetService service = new NetService("", "_monitor_control._tcp", Environment.MachineName, 26908);
+            service.Publish();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CarouselMonitorControl());
+
+            service.Stop();
         }
     }
 }
